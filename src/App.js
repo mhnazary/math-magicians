93063b594ba2cd085043b22ae25818ac5a2b5843
MHNazary <mhnazary2020@gmail.com>
2023-08-15 15:42:33 +0430

    header.jsx created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
 import QuoteComponent from './components/QuoteComponent';
 
 function App() {
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
